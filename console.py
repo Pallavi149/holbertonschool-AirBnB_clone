7ba6b2845eb05cefb8420b09a4b24da42531bd8f
rrarima <5708@holbertonstudents.com>
2023-02-20 20:59:23 +1100

    create interactive terminal

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Console class to"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    intro = 'HolbertonBNB Console.   Type help or ? to list commands.\n'
+    prompt = '(hbnb) '
+    file = None
+
+    # ----- basic hbnb commands -----
+    def do_EOF(self, arg):
+        """Quit the console"""
+        return True
+
+    def do_help(self, arg):
+        """Use help commands"""
+        super().do_help(arg)
+
+    def do_quit(self, arg):
+        """Quit the console"""
+        return self.do_EOF('')
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
