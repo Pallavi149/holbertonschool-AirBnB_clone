81e17787d950d4ea3d6a4aec9e70ce2cb0ab5f36
rrarima <5708@holbertonstudents.com>
2023-02-25 16:21:56 +1100

    added case for save and missing parenthesis

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,24 +4,51 @@ import time
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
-from models.tmp_base_model import BaseModel
-
-class MyTestBaseModel(BaseModel):
-    """Subclass of BaseModel that overrides the save() method"""
-    def save(self):
-        self.updated_at = datetime.utcnow()
+from time import sleep
 
 class TestBaseModel(unittest.TestCase):
     """Test Cases for Base Model"""
 
-    def test_save(self):
-        "Test that save updates update_at with the current datetime"
+    def test_save_method_updates_updated_at_attribute(self):
+        """
+        Tests that calling the save() method updates the 'updated_at' attribute
+        with a new datetime object.
+        """
         model = BaseModel()
         old_updated_at = model.updated_at
         model.save()
         new_updated_at = model.updated_at
         self.assertNotEqual(old_updated_at, new_updated_at)
-        self.assertTrue(isinstance(new_updated_at, datetime))
+        self.assertIsInstance(new_updated_at, datetime)
+        self.assertLess(old_updated_at, new_updated_at)
+
+    def test_save_can_update_updated_at_multiple_times(self):
+        """
+        Tests that calling the save() method multiple times updates the 'updated_at'
+        attribute with a new datetime object each time.
+        """
+        model = BaseModel()
+        old_updated_at = model.updated_at
+        model.save()
+        new_updated_at = model.updated_at
+        self.assertLess(old_updated_at, new_updated_at)
+        sleep(0.01)
+        model.save()
+        newer_updated_at = model.updated_at
+        self.assertLess(new_updated_at, newer_updated_at)
+        self.assertIsInstance(newer_updated_at, datetime)
+
+    def test_save_updates_json_file(self):
+        """
+        Tests that calling the save() method updates the JSON file with a new entry
+        for the instance of the BaseModel class.
+        """
+        model = BaseModel()
+        model.save()
+        model_id = "BaseModel.{}".format(model.id)
+        with open("file.json", "r", encoding="utf-8") as f:
+            file_data = f.read()
+        self.assertIn(model_id, file_data)
 
     def test_to_dict(self):
         """Test to_dict returns a dictionary with all instance attributes"""
